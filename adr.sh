671a4d2aac5e9f62b36dca9fbc3f7dc45a10f9c0
bannaych <bannaych@gmail.com>
2024-10-25 09:35:40 +1100

    Update adr.sh

--- adr.sh
+++ adr.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Script automate the creating on volumes outside the ADR POD using snapshots 
-# Version 0.1
+# Version 0.2
 #
 
 # Set variables
 
-Array1=10.226.224.112
+FA1=10.226.224.112
 POD=ora-target
 PGROUP=adr-rep-pg
 USER=pureuser
 SUFFIX=snap`date +%Y%m%d%M`
 VOL1=drdatavol
 VOL2=drfravol
+DDMONYYYY=`date +%d%b%Y`
+LOG="/home/oracle/adr_${DDMONYYYY}.log"
 
 
 tput clear
@@ -23,6 +25,25 @@ then
 fi
 
 
+function logit ()
+{
+
+   echo "INFO `date`- ${*}" >> $LOG 2>&1
+
+}
+
+RC ()
+{
+ ERR=$?
+   if [ $ERR -ne 1 ]
+       then
+       logit echo "Function had an error please review logs file and contact Administrator"
+       mailx -s "Problem with Refresh Script on ${DBNAME}, please review log file and Contact Administrator " $MAILLIST
+       exit 0
+     fi
+}
+
+
 pgroup ()
 {
 ssh_cmd="$(cat <<-EOF
@@ -30,21 +51,24 @@ ssh_cmd="$(cat <<-EOF
 EOF
 )"
 
-result=`ssh -t $USER@$Array1 $ssh_cmd`
+result=`ssh -t pureuser@10.226.224.112 $ssh_cmd`
 if [[ $result == *"Error"* ]]; then
   echo "It's not there"
-  ssh $USER@$Array1 "purepgroup create $PGROUP"
+  ssh pureuser@10.226.224.112 "purepgroup create $PGROUP"
+  ssh $USER@$FA1 "purevol add drdatavol,drfravol --pgroup $PGROUP"
+  ssh $USER@$FA1 "purepgroup setattr --addtargetlist RedDotC adr-rep-pg"
+
   fi
 
 }
 
 create_snapshot()
 {
-  VOLUMES=$(ssh $USER@$Array1 " purevol list --filter \"name='ora-target::*'\" --csv"|grep -v Name|awk -F:: '{print $2}'|awk -F, '{print $1}')
+  VOLUMES=$(ssh pureuser@10.226.224.112 " purevol list --filter \"name='ora-target::*'\" --csv"|grep -v Name|awk -F:: '{print $2}'|awk -F, '{print $1}')
 for VOL in $VOLUMES; do
     SNAP_NAME="${VOL}.${SNAP_SUFFIX}"
     echo "Taking snapshot of volume $VOL as $SNAP_NAME..."
-    ssh $USER@$Array1 "purevol snap ora-target::$VOL --suffix $SUFFIX"
+    ssh pureuser@10.226.224.112 "purevol snap ora-target::$VOL --suffix $SUFFIX"
 done
 }
 
@@ -55,18 +79,31 @@ ssh_cmd="$(cat <<-EOF
 EOF
 )"
 
-newvol=`ssh -t $USER@$Array1 $ssh_cmd`
+newvol=`ssh -t pureuser@10.226.224.112 $ssh_cmd`
 echo $newvol
 if [[ $newvol == *"Error"* ]]; then
   echo "Volumes are not created"
-  ssh $USER@$Array1 "purevol copy ora-target::data.$SUFFIX drdatavol --overwrite"
-  ssh $USER@$Array1 "purevol copy ora-target::fra.$SUFFIX drfravol --overwrite"
-  ssh $USER@$Array1 " purevol add drdatavol,drfravol --pgroup adr-rep-pg"
+  ssh pureuser@10.226.224.112 "purevol copy ora-target::data.$SUFFIX drdatavol --overwrite"
+  ssh pureuser@10.226.224.112 "purevol copy ora-target::fra.$SUFFIX drfravol --overwrite"
+  ssh pureuser@10.226.224.112 " purevol add drdatavol,drfravol --pgroup adr-rep-pg"
+  else
+  ssh pureuser@10.226.224.112 "purevol copy ora-target::data.$SUFFIX drdatavol --overwrite"
+  ssh pureuser@10.226.224.112 "purevol copy ora-target::fra.$SUFFIX drfravol --overwrite"
   fi
 
+}
+
+replicate ()
+{
+
+
+PGRPSNAP=$(ssh $USER@$FA1 ssh pureuser@10.226.224.112 "purepgroup list adr-rep-pg --snap --csv"|sed '1d'|awk -F, 'NR==1 {print $1}')
+ssh $USER@$FA1 "purepgroup snap $PGROUP"
+ssh $USER@$FA1 "purepgroup send $PGROUP --to RedDotC"
 }
 
 
 pgroup
 create_snapshot
 copy-snap
+replicate
